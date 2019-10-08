375c4429105ec666b6272b863a015a6c14700ea2
CarlosClement12 <carlos.clement@iesdoctorbalmis.com>
2019-10-08 12:56:42 +0200

    Added dynamic generation of numbers

--- EjercicioCalculadoraDiseñoDinamico/MainWindow.xaml.cs
+++ EjercicioCalculadoraDiseñoDinamico/MainWindow.xaml.cs
@@ -23,6 +23,44 @@ namespace EjercicioCalculadoraDiseñoDinamico
         public MainWindow()
         {
             InitializeComponent();
+            AddButtons();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text += ((Button)sender).Tag;
+        }
+
+        private void AddButtons()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int currentButtonValue = (i - 1) * 3 + j + 1;
+
+                    TextBlock textBlock = new TextBlock
+                    {
+                        Text = currentButtonValue.ToString()
+                    };
+                    Viewbox viewbox = new Viewbox
+                    {
+                        Child = textBlock
+                    };
+                    Button button = new Button
+                    {
+                        Tag = currentButtonValue,
+                        Margin = new Thickness(5),
+                        Content = viewbox
+                    };
+
+                    button.SetValue(Grid.RowProperty, i);
+                    button.SetValue(Grid.ColumnProperty, j);
+                    button.Click += new RoutedEventHandler(ButtonClick); 
+                 
+                    MainGrid.Children.Add(button);
+                }
+            }
         }
     }
 }
